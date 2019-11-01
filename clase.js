5386300d6381a0055e67e27410a08ac43a7c719d
andemar <andemartinez98@hotmail.com>
2019-11-01 10:33:48 -0500

    Numeros

---
+++ clase.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+
+//----------STRINGS-----------//
+var nombre = 'Sacha'
+var apellido = 'Asada'
+
+var nombreMayus = nombre.toUpperCase()
+var apellidoMin = apellido.toLowerCase()
+
+var primeraLetraNombre = nombre.charAt(0);
+var cantidadLetrasNombre = nombre.length
+var nombreCompleto = nombre + ' ' + apellido;
+var nombreCompleto2 = `${nombre} ${apellido.toUpperCase()}`
+
+
+var str = nombre.charAt(1) + nombre.charAt(2)
+var str2 = nombre.substr(1,2)
+
+
+//-----------NUMEROS-----------//
+var edad = 27
+
+var precioVino = 200.3
+
+/*Al realizar esta operacion el resultado sera "600.9000000000001"*/
+//var total = precioVino * 
+/*Por tal reazon se realiza de la siguente manera.
+  Esto sirve para 1 decial, pero si son mas, no.*/
+//var total = precioVino * 100 * 3 / 100
+/*Si son mas se usa la siguiente linea*/
+var total = Math.round(precioVino * 100 * 3) / 100
+
+/*Operacion para transformar a string con dos decimales*/
+var totalStr = total.toFixed(3);
+/*Pasar de string a float*/
+var total2 = parseFloat(totalStr)
