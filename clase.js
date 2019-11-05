9d2abba86eb454a37ca958115e1644b05f4652a3
andemar <andemartinez98@hotmail.com>
2019-11-05 10:44:02 -0500

    Alcenace

--- clase.js
+++ clase.js
@@ -2,6 +2,7 @@
 
 
 //----------STRINGS-----------//
+/*
 var nombre = 'Sacha'
 var apellido = 'Asada'
 
@@ -16,27 +17,29 @@ var nombreCompleto2 = `${nombre} ${apellido.toUpperCase()}`
 
 var str = nombre.charAt(1) + nombre.charAt(2)
 var str2 = nombre.substr(1,2)
-
+*/
 
 //-----------NUMEROS-----------//
+/*
 var edad = 27
 
 var precioVino = 200.3
 
-/*Al realizar esta operacion el resultado sera "600.9000000000001"*/
+//Al realizar esta operacion el resultado sera "600.9000000000001"
 //var total = precioVino * 
-/*Por tal reazon se realiza de la siguente manera.
-  Esto sirve para 1 decial, pero si son mas, no.*/
-//var total = precioVino * 100 * 3 / 100
-/*Si son mas se usa la siguiente linea*/
+//Por tal reazon se realiza de la siguente manera.
+//Esto sirve para 1 decial, pero si son mas, no.
+var total = precioVino * 100 * 3 / 100
+//Si son mas se usa la siguiente linea
 var total = Math.round(precioVino * 100 * 3) / 100
 
-/*Operacion para transformar a string con dos decimales*/
+//Operacion para transformar a string con dos decimales
 var totalStr = total.toFixed(3)
-/*Pasar de string a float*/
+//Pasar de string a float
 var total2 = parseFloat(totalStr)
-
+*/
 //-----------CLASE 5-------------//
+/*
 var nombre = 'Sacha', edad = 28
 
 function imprimirEdad(n, e){
@@ -45,10 +48,16 @@ function imprimirEdad(n, e){
 
 
 imprimirEdad()
+*/
+//-----------CLASE 6-------------//
+var nombre = 'Sacha'
 
+function imprimirMayus(n){
+	n = n.toUpperCase()
+	console.log(n)
+}
 
-
-
+imprimirMayus()
 
 
 
